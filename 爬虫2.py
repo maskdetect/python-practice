d028252ad582d3556fb227beb9b9417aac4e40eb
jackhexiangyu12 <87466111+jackhexiangyu12@users.noreply.github.com>
2022-03-10 21:57:47 +0800

    3

--- 爬虫2.py
+++ 爬虫2.py
@@ -1,55 +1,10 @@
-import json
 import random
-import re
-import requests
-import datetime
-from bs4 import BeautifulSoup
-import os
-
-
-def crawl_wiki_data():
-    proxy_list = [
-        {"http": "http://113.195.23.2:9999"},
-        {"http": "http://39.84.114.140:9999"},
-        {"http": "http://110.243.7.29:9999"},
-        {"http": "http://27.188.65.244:8060"}]
-    """
-    爬取百度百科中《乘风破浪的姐姐》中嘉宾信息，返回html
-    """
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-    }
-    url = 'https://baike.baidu.com/item/乘风破浪的姐姐'
-
-    try:
-        response = requests.get(url, headers=headers, proxies=random.choice(proxy_list)).content.decode('utf-8')
-        # 将一段文档传入BeautifulSoup的构造方法,就能得到一个文档的对象, 可以传入一段字符串
-        soup = BeautifulSoup(response.text, 'lxml')
-
-        # 返回所有的<table>所有标签
-        tables = soup.find_all('table')
-        # print(tables)
-        print(len(tables))
-        crawl_table_title = "按姓氏首字母排序"
-        i = 1
-        for table in tables:
-            # 对当前节点前面的标签和字符串进行查找
-            table_titles = table.find_previous('div')
-            # print (1,table_titles)
-            for title in table_titles:
-                if (crawl_table_title in title):
-                    # print (table)
-                    return table
-    except Exception as e:
-        print(e)
-
-
 import json
-import re
 import requests
-import datetime
 from bs4 import BeautifulSoup
 import os
+from matplotlib import pyplot as plt
+import pandas as pd
 
 
 def crawl_wiki_data2():
@@ -70,7 +25,7 @@ def crawl_wiki_data2():
         "x-xsrftoken": "iqWraCpzOAEVVHNZGwDfyaUPzBb7lkuI",  # 反验证功能
         "_xsrf": "697157726143707a4f41455656484e5a47774466796155507a4262376c6b7549"  # 页面校检码，是来检查你是否是从正常的登录页面过来的
     }
-    url = 'https://baike.baidu.com/item/乘风破浪的姐姐'
+    url = 'https://baike.baidu.com/item/乘风破浪的姐姐第二季'
 
     try:
         response = requests.get(url, headers=headers)
@@ -127,7 +82,7 @@ def parse_wiki_data(table_html):
                     stars.append(star)
 
     json_data = json.loads(str(stars).replace("\'", "\""))
-    with open('work/' + 'stars.json', 'w', encoding='UTF-8') as f:
+    with open('work/' + 'stars1.json', 'w', encoding='UTF-8') as f:
         json.dump(json_data, f, ensure_ascii=False)
 
 
@@ -135,7 +90,7 @@ def crawl_everyone_wiki_urls():
     '''
     爬取每个选手的百度百科图片，并保存
     '''
-    with open('work/' + 'stars.json', 'r', encoding='UTF-8') as file:
+    with open('work/' + 'stars1.json', 'r', encoding='UTF-8') as file:
         json_array = json.loads(file.read())
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
@@ -196,88 +151,70 @@ def crawl_everyone_wiki_urls():
         # 将个人信息存储到json文件中
 
         json_data = json.loads(str(star_infos).replace("\'", "\"").replace("\\xa0", ""))
-        with open('work/' + 'stars_info.json', 'w', encoding='UTF-8') as f:
+        with open('work/' + 'stars_info1.json', 'w', encoding='UTF-8') as f:
             json.dump(json_data, f, ensure_ascii=False)
 
 
-def down_save_pic(name,pic_urls):
+def down_save_pic(name, pic_urls):
     '''
     根据图片链接列表pic_urls, 下载所有图片，保存在以name命名的文件夹中,
     '''
-    path = 'work/'+'pics/'+name+'/'
+    path = 'work/' + 'pics/' + name + '/'
     if not os.path.exists(path):
-      os.makedirs(path)
+        os.makedirs(path)
 
     for i, pic_url in enumerate(pic_urls):
         try:
             pic = requests.get(pic_url, timeout=15)
             string = str(i + 1) + '.jpg'
-            with open(path+string, 'wb') as f:
+            with open(path + string, 'wb') as f:
                 f.write(pic.content)
-                #print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
+                # print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
         except Exception as e:
-            #print('下载第%s张图片时失败: %s' % (str(i + 1), str(pic_url)))
+            # print('下载第%s张图片时失败: %s' % (str(i + 1), str(pic_url)))
             print(e)
             continue
 
+
+def draw():
+    # 显示matplotlib生成的图形
+
+    df = pd.read_json('work/stars_info1.json')
+    print(df)
+    weights = df['height']
+    arrs = weights.values
+
+    arrs = [x for x in arrs if not pd.isnull(x)]
+    # pandas.cut用来把一组数据分割成离散的区间。比如有一组年龄数据，可以使用pandas.cut将年龄数据分割成不同的年龄段并打上标签。bins是被切割后的区间.
+    bin = [159, 162, 165, 168, 171, 174]
+    se1 = pd.cut(arrs, bin)
+    print(se1)
+
+    # pandas的value_counts()函数可以对Series里面的每个值进行计数并且排序。
+    pd.value_counts(se1)
+
+    sizes = pd.value_counts(se1)
+    print(sizes)
+    labels = '160~162', '163~165', '166~168', '169~171', '172~174'
+    explode = (0.2, 0.1, 0, 0)
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')
+    plt.savefig('work/result/pie_result02.jpg')
+    plt.show()
+
+
 if __name__ == '__main__':
+    # 爬取百度百科中《乘风破浪的姐姐》中参赛选手信息，返回html
+    htmls = crawl_wiki_data2()
+    print(htmls)
+    # #解析html,得到第一季选手信息，保存为json文件
+    parse_wiki_data(htmls[0])
+    # #从每个选手的百度百科页面上爬取,并保存
+    crawl_everyone_wiki_urls()
 
-     #爬取百度百科中《乘风破浪的姐姐》中参赛选手信息，返回html
-     htmls = crawl_wiki_data2()
-     print(htmls)
-     # #解析html,得到第一季选手信息，保存为json文件
-     parse_wiki_data(htmls[0])
-     # #从每个选手的百度百科页面上爬取,并保存
-     #crawl_everyone_wiki_urls()
+    print("所有信息爬取完成！")
 
-     print("所有信息爬取完成！")
-
-     import matplotlib.pyplot as plt
-     import numpy as np
-     import json
-     import matplotlib.font_manager as font_manager
-
-     # 显示matplotlib生成的图形
-     # matplotlib inline
-
-     with open('work/stars_info.json', 'r', encoding='UTF-8') as file:
-         json_array = json.loads(file.read())
-
-     # 绘制选手年龄分布柱状图,x轴为年龄，y轴为该年龄的小姐姐数量
-     birth_days = []
-     for star in json_array:
-         if 'birth_day' in dict(star).keys():
-             birth_day = star['birth_day']
-             if len(birth_day) == 4:
-                 birth_days.append(birth_day)
-
-     birth_days.sort()
-     print(birth_days)
-
-     birth_days_list = []
-     count_list = []
-     for birth_day in birth_days:
-         if birth_day not in birth_days_list:
-             count = birth_days.count(birth_day)
-             birth_days_list.append(birth_day)
-             count_list.append(count)
-
-     print(birth_days_list)
-     print(count_list)
-
-     # 设置显示中文
-     plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
-     plt.figure(figsize=(15, 8))
-     plt.bar(range(len(count_list)), count_list, color='r', tick_label=birth_days_list,
-             facecolor='#9999ff', edgecolor='white')
-
-     # 这里是调节横坐标的倾斜度，rotation是度数，以及设置刻度字体大小
-     plt.xticks(rotation=45, fontsize=20)
-     plt.yticks(fontsize=20)
-
-     plt.legend()
-     plt.title('''《乘风破浪的姐姐》参赛嘉宾''', fontsize=24)
-     plt.savefig('work/result/bar_result01.jpg')
-     plt.show()
-
-
+    draw()
