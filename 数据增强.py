59ab6001dfa82050a20c48647a456c5ffd5602ac
jackhexiangyu12 <87466111+jackhexiangyu12@users.noreply.github.com>
2022-04-14 22:49:47 +0800

    4

---
+++ 数据增强.py
@@ -0,0 +1,12 @@
+# import numpy as np
+#
+# def rotate_image(img):
+#     """
+#     图像增强
+#     """
+#     angle = np.random.randint(low=-14,high=15)
+#     img=img.rotate(angle)
+#     return img
+#
+# def random_brightness(img):
+#     prob=np.random.uniform(0,1)
